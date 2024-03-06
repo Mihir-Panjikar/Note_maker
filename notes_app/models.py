7e8bc781ddd55a3cbf48d660881bb5975c24130d
Mihir Panjikar <mihirpanjikar@gmail.com>
2024-03-06 19:51:08 +0530

    Created a (Note Maker) notes application using Django (Python Web Framework) which allows for creating, editing and deleting notes.

---
+++ notes_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+# Create your models here.
+
+class note(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    content = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
